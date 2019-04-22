339355e034fa268e1f270f3066e32bfd70f6957d
Gorane7 <kristjankv32@gmail.com>
2019-04-22 22:15:11 +0300

    MongoDB database creation with data.

--- create_mongo.js
+++ create_mongo.js
@@ -1,8 +1,17 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, {useNewUrlParser: true }, function(err, db) {
 	if (err) throw err;
 	console.log("Database created!");
-	db.close();
+	var dbo = db.db("mydb");
+	var phrases = [
+		{ _id: 0, phrase: "Hello!"},
+		{ _id: 1, phrase: "I am not a bot!"}
+	];
+	dbo.collection("phrases").insertMany(phrases, function(err, res) {
+		if (err) throw err;
+		console.log(res);
+		db.close();
+	});
 });
