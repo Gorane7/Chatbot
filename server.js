536797a0135bb0e1facf822a431543745a4a23d2
Gorane7 <kristjankv32@gmail.com>
2019-04-23 11:27:38 +0300

    Express displays data from MongoDB.

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+var db_data;
+
+MongoClient.connect(url, {useNewUrlParser: true }, function(err, db) {
+	if (err) throw err;
+	var dbo = db.db("mydb");
+	dbo.collection("phrases").find({}).toArray(function(err, result) {
+		if (err) throw err;
+		db_data = result;
+		db.close();
+	});
+});
+
 app.get('/', function(req, res) {
-	res.send('Hello world');
+	res.send(db_data[0].phrase + " " + db_data[1].phrase);
 })
 
 var server = app.listen(8081, function() {
