6c17834e2297b431e3538e53f94247ebc57a2dda
Gorane7 <kristjankv32@gmail.com>
2019-04-26 12:05:25 +0300

    App built using Vue VLI

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,12 @@ app.use(cors());
 const languages = require('./routes/api/languages');
 app.use('/api/languages', languages);
 
+if(process.end.NODE_ENV === 'production') {
+	app.use(express.static(__dirname + '/public/'));
+	
+	app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log('Server listening on port %s', port));
